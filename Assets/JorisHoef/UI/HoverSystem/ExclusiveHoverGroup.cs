4f94c8b561bcc5544f09205720d93b19c4472961
Joris Hoevenberg <66425999+JorisHoef@users.noreply.github.com>
2024-08-17 16:28:59 +0200

    change some prefabs, add some new test items in scene, add desciprtion and default hovergroup

--- Assets/JorisHoef/UI/HoverSystem/ExclusiveHoverGroup.cs
+++ Assets/JorisHoef/UI/HoverSystem/ExclusiveHoverGroup.cs
@@ -1,15 +1,29 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace JorisHoef.UI.HoverSystem
 {
+    /// <summary>
+    /// The assigned HoverGroup determines shared OnSelected state
+    /// </summary>
+    /// <remarks>This means that when you assign a hovergroup to an object which has 3 child IHoverables, these 3 children now share 1 shared selected state</remarks>
     public class ExclusiveHoverGroup : MonoBehaviour
     {
+        [Header("If not assigned will use this as Hover Group")]
+        [SerializeField] private Transform _hoverGroup;
+        
         private IHoverable[] _hoverables;
+
+        private void Awake()
+        {
+            if (_hoverGroup == null)
+            {
+                _hoverGroup = this.transform;
+            }
+        }
 
         private void Start()
         {
-            this._hoverables = this.GetComponentsInChildren<IHoverable>();
+            this._hoverables = this._hoverGroup.GetComponentsInChildren<IHoverable>();
             
             foreach (var hoverItem in this._hoverables)
             { 
