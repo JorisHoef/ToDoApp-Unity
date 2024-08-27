2c2bc7a62618fc965047550d9b9756d0d27740ae
Joris <j.hoevenberg@recreate365.com>
2024-08-27 02:18:35 +0200

    cleanup TaskItemManager.cs and add create task functionality

--- Assets/ToDoAppUnity/Tasks/UI/TaskItemManager.cs
+++ Assets/ToDoAppUnity/Tasks/UI/TaskItemManager.cs
@@ -2,19 +2,44 @@
 using System.Collections.Generic;
 using ToDoAppUnity.Models;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace ToDoAppUnity.Tasks.UI
 {
+    /// <summary>
+    /// Manages all UI representations of TaskItems
+    /// </summary>
     public class TaskItemManager : MonoBehaviour
     {
         [SerializeField] private RectTransform _taskItemContainer;
         [SerializeField] private TaskItem _taskItemPrefab;
-
+        [SerializeField] private Button _createTaskButton;
+        
         private readonly TaskApiCaller _taskApiCaller = new TaskApiCaller();
         
         public TaskApiCaller ApiCaller => _taskApiCaller;
         
         private void Awake()
+        {
+            SetupAllTaskDatas();
+        }
+
+        private void OnEnable()
+        {
+            _createTaskButton.onClick.AddListener(OnCreateTaskClicked);
+        }
+
+        private void OnDisable()
+        {
+            _createTaskButton.onClick.RemoveListener(OnCreateTaskClicked);
+        }
+
+        public void DeleteTaskItem(TaskItem taskItem)
+        {
+            GameObject.Destroy(taskItem.gameObject);
+        }
+        
+        private void SetupAllTaskDatas()
         {
             StartCoroutine(_taskApiCaller.GetAllTaskItemsAsync(OnSuccess, OnFail));
             
@@ -33,14 +58,36 @@ namespace ToDoAppUnity.Tasks.UI
         {
             foreach (var taskData in taskDatas)
             {
-                TaskItem newTaskItem = GameObject.Instantiate(_taskItemPrefab, _taskItemContainer);
-                newTaskItem.Initialize(taskData, this);
+                SetupTaskItem(taskData);
+            }
+        }
+        
+        private void OnCreateTaskClicked()
+        {
+            TaskData taskData = new TaskData("New Task", null, null);
+            AddTaskItem(taskData);
+        }
+        
+        private void AddTaskItem(TaskData taskData)
+        {
+            StartCoroutine(_taskApiCaller.PostNewTaskItemAsync(taskData, OnSuccess, OnFail));
+            
+            void OnSuccess(TaskData dataResponse)
+            {
+                Debug.Log($"Successfully created task: {dataResponse.Name}");
+                SetupTaskItem(dataResponse);
+            }
+
+            void OnFail(Exception exception)
+            {
+                Debug.LogError($"Failed to create task item {exception}");
             }
         }
 
-        public void DeleteTaskItem(TaskItem taskitem)
+        private void SetupTaskItem(TaskData taskData)
         {
-            GameObject.Destroy(taskitem.gameObject);
+            TaskItem newTaskItem = GameObject.Instantiate(_taskItemPrefab, _taskItemContainer);
+            newTaskItem.Initialize(taskData, this);
         }
     }
 }
